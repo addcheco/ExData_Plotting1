eb182a8cd79681ad3a58918066353c1c55be06dc
addcheco <63523779+addcheco@users.noreply.github.com>
2020-04-27 14:46:08 -0400

    Png before plot always

--- plot4.R
+++ plot4.R
@@ -6,6 +6,7 @@ importantdata <- reformdata[reformdata$Date %in% c("1/2/2007", "2/2/2007") ,]
 reformdate <- strptime(paste(importantdata$Date, importantdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 #reformat date to day,month,year hour,minute,second
 
+png("plot4.png", width=480, height=480) #export as image
 par(mfrow=c(2,2)) #to start a panel of graphs
 plot(reformdate, NumGlobal, xlab="", ylab="Global Active Power (kilowatts)", type= "l") #topleft graph
 plot(reformdate, importantdata$Voltage, type="l", xlab = "datetime", ylab = "Voltage") #topright graph
@@ -16,5 +17,4 @@ plot(reformdate, importantdata$Sub_metering_1, type="l", ylim=c(0,40), ylab="Ene
   plot(reformdate, importantdata$Sub_metering_3, type="l", col=2, ylim=c(0,40), ylab="Energy sub metering", xlab=" ")
   legend("topright",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
 plot(reformdate, importantdata$Global_reactive_power, type="l", xlab = "datetime", ylab = "Global_reactive_power") #bottomright graph
-png("plot4.png", width=480, height=480) #export as image
 dev.off() #sign out
