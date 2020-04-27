b50c84d98063a063d63828a74b173feb6047339c
addcheco <63523779+addcheco@users.noreply.github.com>
2020-04-27 14:45:38 -0400

    same problem as plot2

--- plot3.R
+++ plot3.R
@@ -4,6 +4,7 @@ reformdata <- read.table(data, header=TRUE, sep=";", stringsAsFactors = FALSE, d
 importantdata <- reformdata[reformdata$Date %in% c("1/2/2007", "2/2/2007") ,]
 #only from Feb1-Feb2
 
+png("plot3.png", width=480, height=480) #export as image
 reformdate <- strptime(paste(importantdata$Date, importantdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 #reformat date to day,month,year hour,minute,second
 plot(reformdate, importantdata$Sub_metering_1, type="l", ylim=c(0,40), ylab="Energy sub metering", xlab=" ")
@@ -12,5 +13,4 @@ plot(reformdate, importantdata$Sub_metering_2, type="l", col=4, ylim=c(0,40), yl
 par(new=TRUE)
 plot(reformdate, importantdata$Sub_metering_3, type="l", col=2, ylim=c(0,40), ylab="Energy sub metering", xlab=" ")
 legend("topright",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
-png("plot3.png", width=480, height=480) #export as image
 dev.off() #sign out
